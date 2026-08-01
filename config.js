55fafbe78b854bf3a5efc24331dbb5513cd5b1f8
tdrsayla-dev <t.drsayla@stkgroupe.com>
2026-08-01 10:48:09 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
-// Configuration variables for Supabase connection
-// WARNING: Do not commit this file with production credentials to public repositories!
 const CONFIG = {
     SUPABASE_URL: "https://qqyotianqcgpdzrfgjif.supabase.co",
-    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxeW90aWFucWNncGR6cmZnamlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwNDQ1MzcsImV4cCI6MjA5ODYyMDUzN30.6VlHbimuMkg0UtxCw2cbU5mN_c9zPmizNuzrL5tCtME"
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxeW90aWFucWNncGR6cmZnamlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwNDQ1MzcsImV4cCI6MjA5ODYyMDUzN30.6VlHbimuMkg0UtxCw2cbU5mN_c9zPmizNuzrL5tCtME",
+    MLM_SUPABASE_URL: "https://mfpkeyrykqnrywyksyqp.supabase.co",
+    MLM_SUPABASE_ANON_KEY: "sb_publishable_807NIkuj6MAs1KZY-m4tug_Fm1Mk-AO"
 };
 
 window.SUPABASE_URL = CONFIG.SUPABASE_URL;
 window.SUPABASE_ANON_KEY = CONFIG.SUPABASE_ANON_KEY;
 window.SUPABASE_REST_URL = `${CONFIG.SUPABASE_URL.replace(/\/$/, '')}/rest/v1`;
+window.MLM_SUPABASE_URL = CONFIG.MLM_SUPABASE_URL;
+window.MLM_SUPABASE_ANON_KEY = CONFIG.MLM_SUPABASE_ANON_KEY;
